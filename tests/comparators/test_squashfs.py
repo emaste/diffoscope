159c37524102108d6215b83db4f0b5506f781c52
Chris Lamb <lamby@debian.org>
2019-08-25 10:54:53 +0100

    Apply "black" to the tests, not just to the "diffocope" module.
    Gbp-Dch: ignore

--- tests/comparators/test_squashfs.py
+++ tests/comparators/test_squashfs.py
@@ -61,7 +61,9 @@ def differences(squashfs1, squashfs2):
     return squashfs1.compare(squashfs2).details
 
 
-@skip_unless_tool_is_at_least('unsquashfs', unsquashfs_version, '4.3+git190823')
+@skip_unless_tool_is_at_least(
+    'unsquashfs', unsquashfs_version, '4.3+git190823'
+)
 def test_superblock(differences):
     expected_diff = get_data('squashfs_superblock_expected_diff')
     assert differences[0].unified_diff == expected_diff
