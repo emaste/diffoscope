f14a0c64ec9b1f3e31e1273f6215e908a3c8cbba
Chris Lamb <lamby@debian.org>
2020-06-09 11:16:32 +0100

    Place the "# noqa" comment in the right place when ignoring some unused imports.

--- diffoscope/presenters/html/__init__.py
+++ diffoscope/presenters/html/__init__.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
 
-from .html import (
+from .html import ( # noqa
     HTMLPresenter,
     HTMLDirectoryPresenter,
     JQUERY_SYSTEM_LOCATIONS,
-)  # noqa
+)
