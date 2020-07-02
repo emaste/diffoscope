48f9ac7f43f737208238ed24d437ba64b1c4375a
Jean-Romain Garnier <salsa@jean-romain.com>
2020-07-02 16:49:44 +0200

    Update decompiler license string

--- diffoscope/comparators/utils/decompile.py
+++ diffoscope/comparators/utils/decompile.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# diffoscope: in-depth comparison of files, archives, and directories
+#
+# Copyright © 2020 Jean-Romain Garnier <salsa@jean-romain.com>
+#
+# diffoscope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffoscope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import sys
 import abc
@@ -123,7 +142,7 @@ class DecompileRadare2(Decompile):
 
     @tool_required("radare2")
     def _decompile(self):
-        self._stdout = self._run_r2_command().strip()
+        self._stdout = self._run_r2_command()
 
 
 class AsmFunction(File):
