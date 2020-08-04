6d64a0a7d4c11b00725a890855d7ca884fb365e1
Chris Lamb <lamby@debian.org>
2020-08-04 10:35:33 +0100

    Also apply 'Black' to setup.py.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3, 7):
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
     def initialize_options(self):
         super().initialize_options()
@@ -29,59 +29,52 @@ class PyTest(TestCommand):
     def run_tests(self):
         # Inline import, otherwise the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
 setup(
-    name='diffoscope',
+    name="diffoscope",
     version=diffoscope.VERSION,
-    description='in-depth comparison of files, archives, and directories',
-    long_description=open('README.rst', encoding='utf-8').read(),
-    long_description_content_type='text/x-rst',
-    author='Diffoscope developers',
-    author_email='diffoscope@lists.reproducible-builds.org',
-    license='GPL-3+',
-    url='https://diffoscope.org/',
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    entry_points={
-        'console_scripts': [
-            'diffoscope=diffoscope.main:main'
-        ],
-    },
-    install_requires=[
-        'python-magic',
-        'libarchive-c',
-    ],
+    description="in-depth comparison of files, archives, and directories",
+    long_description=open("README.rst", encoding="utf-8").read(),
+    long_description_content_type="text/x-rst",
+    author="Diffoscope developers",
+    author_email="diffoscope@lists.reproducible-builds.org",
+    license="GPL-3+",
+    url="https://diffoscope.org/",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
+    entry_points={"console_scripts": ["diffoscope=diffoscope.main:main"],},
+    install_requires=["python-magic", "libarchive-c",],
     extras_require={
-        'distro_detection': ['distro'],
-        'cmdline': ['argcomplete', 'progressbar'],
-        'comparators': [
-            'binwalk',
-            'defusedxml',
-            'guestfs',
-            'jsondiff',
-            'python-debian',
-            'pypdf2',
-            'pyxattr',
-            'rpm-python',
-            'tlsh',
+        "distro_detection": ["distro"],
+        "cmdline": ["argcomplete", "progressbar"],
+        "comparators": [
+            "binwalk",
+            "defusedxml",
+            "guestfs",
+            "jsondiff",
+            "python-debian",
+            "pypdf2",
+            "pyxattr",
+            "rpm-python",
+            "tlsh",
         ],
     },
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
     ],
-
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#project-urls
     project_urls={
         "Issues": "https://salsa.debian.org/reproducible-builds/diffoscope/-/issues",
