a615cc31a6e6d767a388ad3e074fc828703419d8
Chris Lamb <lamby@debian.org>
2020-11-23 11:03:04 +0000

    Don't depends on radare2 in the Debian autopkgtests; it will not be in bullseye due to security considerations (see #950372). (Closes: #975313)

--- debian/tests/control.sh
+++ debian/tests/control.sh
@@ -15,6 +15,8 @@ sed -i "s#%PYRECOMMENDS%#$(python3 -c "import distutils.core; \
 	print(', '.join(sorted(['python3-{}'.format(x) for y in setup.extras_require.values() for x in y])))" \
 )#" debian/tests/control.tmp
 
+# Don't test-depend on radare2; not in bullseye for security reasons. (#950372)
+sed -i "s#radare2, ##" debian/tests/control.tmp
 
 sed -i "s,python3-python-debian,python3-debian," debian/tests/control.tmp
 sed -i "s,python3-rpm-python,python3-rpm," debian/tests/control.tmp
