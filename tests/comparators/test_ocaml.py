1fcc0d51994ab4fd063fed84dd65ac6ff99877ec
Chris Lamb <lamby@debian.org>
2021-09-12 11:20:10 +0100

    Update tests for OCaml 4.12. Thanks to Zbigniew Jędrzejewski-Szmek for the report. (Closes: reproducible-builds/diffoscope#274)

--- tests/comparators/test_ocaml.py
+++ tests/comparators/test_ocaml.py
@@ -1,7 +1,7 @@
 #
 # diffoscope: in-depth comparison of files, archives, and directories
 #
-# Copyright © 2018-2020 Chris Lamb <lamby@debian.org>
+# Copyright © 2018-2021 Chris Lamb <lamby@debian.org>
 #
 # diffoscope is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -56,7 +56,7 @@ def ocaml_version():
     return out.decode("utf-8").split()[-1]
 
 
-@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.11.1")
+@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.12")
 def test_identification(cmi1):
     assert isinstance(cmi1, OcamlInterfaceFile)
 
@@ -66,17 +66,17 @@ def differences(cmi1, cmi2):
     return cmi1.compare(cmi2).details
 
 
-@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.11.1")
+@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.12")
 def test_no_differences(cmi1):
     difference = cmi1.compare(cmi1)
     assert difference is None
 
 
-@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.11.1")
+@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.12")
 def test_diff(differences):
     assert_diff(differences[0], "ocaml_expected_diff")
 
 
-@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.11.1")
+@skip_unless_tool_is_at_least("ocamlobjinfo", ocaml_version, "4.12")
 def test_compare_non_existing(monkeypatch, cmi1):
     assert_non_existing(monkeypatch, cmi1, has_null_source=False)
