3f09bbc75bffdd178c4d8f25e1669224543c74ab
Mattia Rizzolo <mattia@debian.org>
2021-11-18 19:57:46 +0100

    It looks like we support both python 3.9 and 3.10, document the fact.
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Utilities",
     ],
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#project-urls
