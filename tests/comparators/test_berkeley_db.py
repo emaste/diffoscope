ab87ab6a62d654551da022160ed931e73154691d
Jelle van der Waa <jelle@archlinux.org>
2022-12-29 16:08:54 +0100

    Support Berkeley DB 6

--- tests/comparators/test_berkeley_db.py
+++ tests/comparators/test_berkeley_db.py
@@ -16,16 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
 
+import subprocess
 import pytest
 
 from diffoscope.comparators.berkeley_db import BerkeleyDBFile
 
 from ..utils.data import load_fixture, assert_diff
-from ..utils.tools import skip_unless_tools_exist
+from ..utils.tools import (
+    skip_unless_tools_exist,
+    skip_unless_tool_is_at_least,
+    skip_unless_tool_is_at_most,
+)
 from ..utils.nonexisting import assert_non_existing
 
 db1 = load_fixture("test1.db")
 db2 = load_fixture("test2.db")
+
+
+def db_version():
+    try:
+        out = subprocess.check_output(["db_dump", "-V"])
+    except subprocess.CalledProcessError as e:
+        out = e.output
+    return out.decode("UTF-8").split()[2].replace(":", "")
 
 
 def test_identification(db1):
@@ -43,8 +56,15 @@ def test_no_differences(db1):
 
 
 @skip_unless_tools_exist("db_dump")
+@skip_unless_tool_is_at_most("db_dump", db_version, "6.0")
 def test_diff(differences):
     assert_diff(differences[0], "berkeley_db_expected_diff")
+
+
+@skip_unless_tools_exist("db_dump")
+@skip_unless_tool_is_at_least("db_dump", db_version, "6.0")
+def test_diff6(differences):
+    assert_diff(differences[0], "berkeley6_db_expected_diff")
 
 
 @skip_unless_tools_exist("db_dump")
