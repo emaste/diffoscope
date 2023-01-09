fca2293a89a066944bbd16546ad0c324e4d49e22
Chris Lamb <lamby@debian.org>
2023-01-09 08:14:41 +0000

    Drop debugging code.

--- tests/comparators/test_icc.py
+++ tests/comparators/test_icc.py
@@ -54,7 +54,6 @@ def cd_iccdump_version():
         val = subprocess.check_output(
             ("cd-iccdump", data("test1.icc"))
         ).decode("utf-8")
-        raise subprocess.CalledProcessError(0, cmd=("123",))
     except subprocess.CalledProcessError as exc:
         if exc.returncode != 0:
             raise
